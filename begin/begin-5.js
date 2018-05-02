96103595bc2880cbed551f4339782031d145f25e
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-05-02 22:36:22 +0300

    Task begin5 was solve.

---
+++ begin/begin-5.js
@@ -0,0 +1,7 @@
+//Дана длина ребра куба а. Найти объем куба V = a*a*a и площадь его поверхности S = 6*a*a.
+
+const getVolAndSquare = a => {
+    let vol = a*a*a;
+    let sq = 6 * a * a;
+    console.log('Volume is: ' + vol + '\n' + 'square is: ' + sq);
+}
