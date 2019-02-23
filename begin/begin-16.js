be970078ffef7035d13a350a18ee1a1200fc4182
Soloviov Dmytro <d.v.solovyov1@gmail.com>
2019-02-23 18:42:51 +0200

    Math.abs method added. Change output method.

--- begin/begin-16.js
+++ begin/begin-16.js
@@ -1,6 +1,8 @@
 // Найти расстояние между двумя точками с задаными координатами x1 и x2 на числовой оси |x2 - x1|
 
 const getDistance = (x1, x2) => {
-  abs = x2 - x1;
-  return abs;
+  let value = Math.abs(x2 - x1);
+  return `Distance is: ${value}`;
 }
+
+console.log(getDistance(100, 78))
